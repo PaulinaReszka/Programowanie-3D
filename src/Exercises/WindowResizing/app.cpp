5299d9aa184e5b94c0dc6834b505059a02ac1680
Paulina Reszka <pau.reszka@gmail.com>
2020-11-25 10:50:56 +0100

    Window Resizing upload

---
+++ src/Exercises/WindowResizing/app.cpp
@@ -0,0 +1,171 @@
+//
+// Created by pbialas on 25.09.2020.
+//
+
+#include "app.h"
+
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+
+#include "Application/utils.h"
+
+void SimpleShapeApplication::init() {
+    std::vector<GLushort> indices = {
+            0, 1, 2,
+            3, 4, 5,
+            8, 7, 9,
+            9, 10, 11,
+            5, 4, 6,
+            14, 13, 15
+    };
+
+
+    auto program = xe::create_program(std::string(PROJECT_DIR) + "/shaders/base_vs.glsl",
+                                      std::string(PROJECT_DIR) + "/shaders/base_fs.glsl");
+
+
+    if (!program) {
+        std::cerr << "Cannot create program from " << std::string(PROJECT_DIR) + "/shaders/base_vs.glsl" << " and ";
+        std::cerr << std::string(PROJECT_DIR) + "/shaders/base_fs.glsl" << " shader files" << std::endl;
+    }
+
+    std::vector<GLfloat> vertices = {
+            0.5f, 0.0f, -0.5f, 0.5, 0.4, 0.1,
+            0.5f, 0.0f, 0.5f, 0.5, 0.4, 0.1,
+            -0.5f, 0.0f, 0.5f, 0.5, 0.4, 0.1,
+            -0.5f, 0.0f, -0.5f, 0.5, 0.4, 0.1,
+
+            0.5f, 0.0f, -0.5f, 0.3, 0.2, 0.7,
+            0.5f, 0.0f, 0.5f, 0.3, 0.2, 0.7,
+            0.0f, 1.0f, 0.0f, 0.3, 0.2, 0.7,
+
+            0.5f, 0.0f, 0.5f, 0.8, 0.3, 0.1,
+            -0.5f, 0.0f, 0.5f, 0.8, 0.3, 0.1,
+            0.0f, 1.0f, 0.0f, 0.8, 0.3, 0.1,
+
+            -0.5f, 0.0f, 0.5f, 0.7, 0.6, 0.3,
+            -0.5f, 0.0f, -0.5f, 0.7, 0.6, 0.3,
+            0.0f, 1.0f, 0.0f, 0.7, 0.6, 0.3,
+
+            -0.5f, 0.0f, -0.5f, 0.8, 0.2, 0.8,
+            0.5f, 0.0f, -0.5f, 0.8, 0.2, 0.8,
+            0.0f, 1.0f, 0.0f, 0.8, 0.2, 0.8,
+    };
+
+    GLuint idx_buffer_handle;
+    glGenBuffers(1, &idx_buffer_handle);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx_buffer_handle);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLushort), indices.data(),
+                 GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    GLuint v_buffer_handle;
+    glGenBuffers(1, &v_buffer_handle);
+    glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+
+    glGenVertexArrays(1, &vao_);
+    glBindVertexArray(vao_);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx_buffer_handle);
+    glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(0));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(3 * sizeof(GLfloat)));
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    auto u_modifiers_index = glGetUniformBlockIndex(program, "Modifiers");
+    if (u_modifiers_index == GL_INVALID_INDEX) {
+        std::cout << "Cannot find Modifiers uniform block in program" << std::endl;
+    } else {
+        glUniformBlockBinding(program, u_modifiers_index, 0);
+    }
+    float strength = 1.5;
+    float light[3] = {0.7, 0.5, 0.8};
+
+    glm::mat4 M(1.0f);
+    glm::mat4 view = glm::lookAt(glm::vec3(7.0f, 6.0f,  1.2f),
+                                 glm::vec3(0.5f, 0.0f, 0.5f),
+                                 glm::vec3(0.7f, 1.0f, 0.5f));
+
+    glm::mat4 projectionMatrix = glm::perspective(
+            glm::radians(20.f),
+            320.0f / 258.0f,
+            0.1f,
+            100.0f
+    );
+    glm::mat4 PVM = projectionMatrix * view * M;
+
+    GLuint ubo_handle(0u);
+    glGenBuffers(1, &ubo_handle);
+    glBindBuffer(GL_UNIFORM_BUFFER, ubo_handle);
+    glBufferData(GL_UNIFORM_BUFFER, 8 * sizeof(float), nullptr, GL_STATIC_DRAW);
+    glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(float), &strength);
+    glBufferSubData(GL_UNIFORM_BUFFER, 4 * sizeof(float), 3 * sizeof(float), light);
+    glBindBuffer(GL_UNIFORM_BUFFER, 0);
+    glBindBufferBase(GL_UNIFORM_BUFFER, 0, ubo_handle);
+
+
+    GLuint ubo_handle_pvm(0u);
+    glGenBuffers(1, &ubo_handle_pvm);
+    glBindBuffer(GL_UNIFORM_BUFFER, ubo_handle_pvm);
+    glBufferData(GL_UNIFORM_BUFFER, sizeof(glm::mat4), nullptr, GL_STATIC_DRAW);
+    glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(glm::mat4), &PVM[0]);
+    glBindBuffer(GL_UNIFORM_BUFFER, 1);
+    glBindBufferBase(GL_UNIFORM_BUFFER, 1, ubo_handle_pvm);
+
+    auto u_transformations_index = glGetUniformBlockIndex(program, "Transformations");
+    if (u_transformations_index == GL_INVALID_INDEX) {
+        std::cout << "Cannot find Transformations uniform block in program" << std::endl;
+    } else {
+        glUniformBlockBinding(program, u_transformations_index, 1);
+    }
+
+    glClearColor(0.81f, 0.81f, 0.8f, 1.0f);
+    int w, h;
+    std::tie(w, h) = frame_buffer_size();
+    glViewport(0, 0, w, h);
+
+    glEnable(GL_DEPTH_TEST);
+    glUseProgram(program);
+
+    glEnable(GL_CULL_FACE);
+    glFrontFace(GL_CCW);
+    glCullFace(GL_BACK);
+
+
+    aspect_ = (float)w/h;
+    fov_ = glm::pi<float>()/4.0;
+    near_ = 0.1f;
+    far_ = 100.0f;
+    P_ = glm::perspective(fov_, aspect_, near_, far_);
+    V_ = glm::lookAt(glm::vec3(7.0f, 6.0f,  1.2f),
+                     glm::vec3(0.5f, 0.0f, 0.5f),
+                     glm::vec3(0.7f, 1.0f, 0.5f));
+
+}
+
+void SimpleShapeApplication::frame() {
+    auto PVM = P_ * V_;
+    glBindBuffer(GL_UNIFORM_BUFFER, u_pvm_buffer_);
+    glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(glm::mat4), &PVM[0]);
+    glBindBuffer(GL_UNIFORM_BUFFER, 0);
+
+    glBindVertexArray(vao_);
+    glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_SHORT, reinterpret_cast<GLvoid *>(0));
+    glBindVertexArray(0);
+}
+
+void SimpleShapeApplication::framebuffer_resize_callback(int w, int h) {
+    Application::framebuffer_resize_callback(w, h);
+    glViewport(0,0,w,h);
+    aspect_ = (float) w / h;
+    P_ = glm::perspective(fov_, aspect_, near_, far_);
+}
